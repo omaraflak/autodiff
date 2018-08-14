bdfa055ab1ed40fcc0efea97061a39e0828099d4
Omar Aflak <aflakomar@gmail.com>
2018-08-14 11:49:06 +0200

    optimized uid generator

--- root/include/utils.h
+++ root/include/utils.h
@@ -5,7 +5,12 @@
 #include <random>
 
 namespace utils {
+    static std::string allowed_chars = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    static std::uniform_int_distribution<> dis(0, allowed_chars.size()-1);
+
     std::string uid(size_t length=16);
-} /* utils */
+}
 
 #endif /* end of include guard: UTILS_H */
